84324b34bff8c8991688494d8ce135c1a25c78d5
Cheng Liu <chengcyber@users.noreply.github.com>
2024-02-20 17:09:36 -0800

    feat: remove sparo sparse and sparo sparse-add

--- apps/sparo-lib/src/cli/commands/cmd-list.ts
+++ apps/sparo-lib/src/cli/commands/cmd-list.ts
@@ -1,9 +1,7 @@
 import { Constructable } from '../../di/types';
 import { CloneCommand } from './clone';
 import { HelpCommand } from './help';
-import { SparseCommand } from './sparse';
 import { SparseListCommand } from './sparse-list';
-import { SparseAddCommand } from './sparse-add';
 import { AutoConfigCommand } from './auto-config';
 import { FetchCommand } from './fetch';
 import { PurgeCommand } from './purge';
@@ -13,10 +11,8 @@ import { CICloneCommand } from './ci-clone';
 
 export const COMMAND_LIST: Constructable[] = [
   CloneCommand,
-  SparseCommand,
   HelpCommand,
   SparseListCommand,
-  SparseAddCommand,
   AutoConfigCommand,
   FetchCommand,
   PurgeCommand
