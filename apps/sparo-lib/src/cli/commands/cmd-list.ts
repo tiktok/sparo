3422d10d9e35f61df95dcddc3bdea8085fb04347
Cheng Liu <chengcyber@outlook.com>
2024-02-23 15:21:23 -0800

    Update apps/sparo-lib/src/cli/commands/cmd-list.ts
    Co-authored-by: Pete Gonzalez <4673363+octogonz@users.noreply.github.com>

--- apps/sparo-lib/src/cli/commands/cmd-list.ts
+++ apps/sparo-lib/src/cli/commands/cmd-list.ts
@@ -14,6 +14,8 @@ import { GitFetchCommand } from './git-fetch';
 import { GitPullCommand } from './git-pull';
 import { InitProfileCommand } from './init-profile';
 
+// When adding new Sparo subcommands, remember to update this doc page:
+// https://github.com/tiktok/sparo/blob/main/apps/website/docs/pages/commands/overview.md
 export const COMMAND_LIST: Constructable[] = [
   HelpCommand,
   AutoConfigCommand,
