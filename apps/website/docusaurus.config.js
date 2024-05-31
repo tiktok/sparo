84b51f2e649d56459f779ee58cca7b9d644ce406
Cheng Liu <chengcyber@users.noreply.github.com>
2024-05-31 14:19:53 -0700

    chore: setup preview action

--- apps/website/docusaurus.config.js
+++ apps/website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://tiktok.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sparo/',
+  baseUrl: process.env.BASE_URL || '/sparo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
